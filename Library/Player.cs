74d8125811d83f772a00c487a592bbf56a82c7da
Samuele Ingrassia <samuele.ingrassia@studenti.ittsrimini.edu.it>
2023-01-16 10:34:01 +0100

    Aggiungere i file di progetto.

---
+++ Library/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library
+{
+    public class Player
+    {
+        string name;
+        string surname;
+        ERole role;
+
+        public enum ERole 
+        { 
+            Rosa,
+            Riserva
+        }
+        public Player(string name, string surname, ERole role)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.role = role;
+        }
+
+        public string Description()
+        {
+            return $"{surname} {name}";
+        }
+
+        public ERole Role { 
+            // TODO P.3
+        }
+
+    }
+}
