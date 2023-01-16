0aac2d1036d08b3f0736e7f0980a45b62db546e6
Samuele Ingrassia <samuele.ingrassia@studenti.ittsrimini.edu.it>
2023-01-16 11:06:30 +0100

    feat: Add AddPlayers() Test in UnitTest.cs

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -12,6 +12,25 @@ namespace UnitTest
         [Test]
         public void AddPlayers()
         {
+            Team team = new Team("Squadra 1");
+            Assert.That(team.AddPlayer(new Player("Cognome 1", "Nome 1", Player.ERole.Rosa)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 2", "Nome 2", Player.ERole.Rosa)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 3", "Nome 3", Player.ERole.Rosa)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 4", "Nome 4", Player.ERole.Rosa)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 5", "Nome 5", Player.ERole.Rosa)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 6", "Nome 6", Player.ERole.Rosa)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 7", "Nome 7", Player.ERole.Rosa)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 8", "Nome 8", Player.ERole.Rosa)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 9", "Nome 9", Player.ERole.Rosa)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 10", "Nome 10", Player.ERole.Rosa)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 11", "Nome 11", Player.ERole.Rosa)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 11", "Nome 11", Player.ERole.Rosa)), Is.False);
+
+            Assert.That(team.AddPlayer(new Player("Cognome 12", "Nome 12", Player.ERole.Riserva)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 13", "Nome 13", Player.ERole.Riserva)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 14", "Nome 14", Player.ERole.Riserva)), Is.True);
+            Assert.That(team.AddPlayer(new Player("Cognome 11", "Nome 11", Player.ERole.Rosa)), Is.False);
+
         }
 
         [Test]
